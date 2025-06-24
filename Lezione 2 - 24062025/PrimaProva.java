677d78e9867eed63d49f6458ebd9d4e2b4b7a383
Massyiwnl <87180371+Massyiwnl@users.noreply.github.com>
2025-06-24 17:47:16 +0200

    Add files via upload

---
+++ Lezione 2 - 24062025/PrimaProva.java
@@ -0,0 +1,8 @@
+public class PrimaProva {
+    public static void main(String[] args) {
+        String massi = "Massi";
+        System.out.println("Hi, ");
+        System.out.println(massi);
+        System.out.println("How are you?");
+    }
+}
