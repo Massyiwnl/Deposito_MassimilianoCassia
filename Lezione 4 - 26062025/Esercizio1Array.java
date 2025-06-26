6b53d7eaf08c2e98c04178c883a7559042061042
Massyiwnl <CSSMSM01B07I754Z@studium.unict.it>
2025-06-26 15:52:21 +0200

    Esercizio1Array.java

--- Lezione 4 - 26062025/Esercizio1Array.java
+++ Lezione 4 - 26062025/Esercizio1Array.java
@@ -6,9 +6,8 @@ public class Esercizio1Array {
         final int MAX_DOLCI = 10; // Massimo numero di dolci diversi
         String[] nomiDolci = new String[MAX_DOLCI]; // Array per i nomi dei dolci
         int[] quantitaDolci = new int[MAX_DOLCI];   // Array per le quantità ordinate
-        
         Scanner scanner = new Scanner(System.in);
-        
+        Scanner scanner2 = new Scanner(System.in); //Uso duo scanner per evitare conflitti vari
         System.out.print("Quanti dolci diversi vuoi ordinare? (max 10): ");
         int numDolci = scanner.nextInt();
         
@@ -17,14 +16,10 @@ public class Esercizio1Array {
             System.out.print("Numero non valido, inserisci un numero tra 1 e 10: ");
             numDolci = scanner.nextInt();
         }
-        
-        scanner.nextLine(); // Consuma il newline rimasto
-        
         // Raccolta dati
         for (int i = 0; i < numDolci; i++) {
             System.out.print("Inserisci il nome del dolce n. " + (i + 1) + ": ");
-            nomiDolci[i] = scanner.nextLine();
-            
+            nomiDolci[i] = scanner2.nextLine();
             System.out.print("Inserisci la quantità ordinata per " + nomiDolci[i] + ": ");
             int quantita = scanner.nextInt();
             
@@ -35,13 +30,10 @@ public class Esercizio1Array {
             }
             
             quantitaDolci[i] = quantita;
-            scanner.nextLine(); // Consuma il newline rimasto
         }
-        
         // Stampa degli ordini effettuati
         System.out.println("\n Riepilogo degli ordini effettuati");
         int totaleDolci = 0;
-        
         for (int i = 0; i < numDolci; i++) {
             System.out.println(nomiDolci[i] + ": " + quantitaDolci[i] + " pezzi");
             totaleDolci += quantitaDolci[i];
