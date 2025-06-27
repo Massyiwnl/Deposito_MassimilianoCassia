9bb6b8b8e0d41e36ac7a9af524cbca6aef01a5cd
Massyiwnl <CSSMSM01B07I754Z@studium.unict.it>
2025-06-27 17:23:42 +0200

    Update Esercizio1Funzioni.java

--- Lezione 5 - 27062025/Esercizio1Funzioni.java
+++ Lezione 5 - 27062025/Esercizio1Funzioni.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
-
+//modificare la funzione della stringa andando a conteggiare quante char ci sono in quella stringa
 public class Esercizio1Funzioni {
 
     public static int Fattoriale(int n) {
         int risultato = 1;
-        for (int i = 1; i <= n; i++) { //vado a fare il fattoriale
+        for (int i = 1; i <= n; i++) { // vado a fare il fattoriale
             risultato = risultato * i;
         }
         return risultato;
     }
 
     public static void Fattoriale(String s) {
-        System.out.println("Inserisci un numero valido");
+        int lunghezza = s.length(); // conteggio dei caratteri
+        System.out.println("Hai inserito una parola di " + lunghezza + " caratteri.");
     }
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         // Esempio semplice: o scrivo direttamente un numero o una parola nel codice
-
-        System.out.println("Esempio con numero:");
-        Fattoriale(5);
-
-        System.out.println("Esempio con parola:");
+        System.out.println("Esempio con numero 5: " + Fattoriale(5));
+        System.out.println("Esempio con parola ciao:");
         Fattoriale("ciao");
     }
 }
