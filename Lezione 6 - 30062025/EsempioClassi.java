a74a154de74e0546386b85cb9f20f8b9a288da59
Massyiwnl <CSSMSM01B07I754Z@studium.unict.it>
2025-06-30 12:02:27 +0200

    Update EsempioClassi.java

--- Lezione 6 - 30062025/EsempioClassi.java
+++ Lezione 6 - 30062025/EsempioClassi.java
@@ -10,7 +10,16 @@ class Auto{
     }
 }
 
+class Studente{
+    String nome;
+    static int totaleStudenti = 0; // Variabile statica per tenere traccia del numero totale di studenti
 
+    Studente(String nome) { //costruttore della classe Studente
+        // Il costruttore viene chiamato quando si crea un nuovo oggetto Studente
+        this.nome = nome; // Inizializza il nome dello studente
+        totaleStudenti++; // Incrementa il conteggio degli studenti ogni volta che viene creato un nuovo oggetto Studente
+    } 
+}
 
 public class EsempioClassi {
     public static void main(String[] args) {
@@ -27,5 +36,14 @@ public class EsempioClassi {
         auto2.prezzo = 20000.0; // Assegnazione del valore alla proprietà prezzo
 
         auto2.mostraInfo(); // Chiamata al metodo mostraInfo per visualizzare le informazioni della seconda auto
+
+        // Creazione di oggetti Studente
+        Studente studente1 = new Studente("Alice"); // Creazione di un oggetto Studente con nome "Alice"
+        Studente studente2 = new Studente("Bob"); // Creazione di un altro oggetto Studente con nome "Bob"
+        Studente studente3 = new Studente("Charlie"); // Creazione di un altro oggetto Studente con nome "Charlie"
+        // Stampa del numero totale di studenti creati
+        System.out.println("Totale studenti: " + Studente.totaleStudenti);
+        // Il numero totale di studenti è una variabile statica, quindi può essere accessibile senza creare un oggetto Studente
+        // In questo caso, Studente.totaleStudenti è accessibile direttamente dalla classe  Studente, senza bisogno di un'istanza specifica.
     }
 }
