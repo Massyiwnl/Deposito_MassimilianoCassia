5bd8a72b26b8e782d822deaddd9c22f94b38e7b1
Massyiwnl <CSSMSM01B07I754Z@studium.unict.it>
2025-07-16 18:08:13 +0200

    esempio jdbc

---
+++ Week 4/Altra sintassi SQL/ProgettoJDBC/ProgettoJDBC/src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/sakila"; // <-- Usa 'sakila' come DB
+        String user = "root";      // Cambia se usi un altro utente
+        String password = "Root123!";      // Metti la password giusta
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Connessione riuscita!");
+        } catch (SQLException e) {
+            System.out.println("Errore nella connessione");
+            e.printStackTrace();
+        }
+    }
+}
