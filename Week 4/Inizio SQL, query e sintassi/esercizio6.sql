adb2305e87220e5fd5505ff33c428568b2b60c8d
Massyiwnl <CSSMSM01B07I754Z@studium.unict.it>
2025-07-16 09:41:29 +0200

    Update esercizio6.sql

--- Week 4/Inizio SQL, query e sintassi/esercizio6.sql
+++ Week 4/Inizio SQL, query e sintassi/esercizio6.sql
@@ -1,6 +1,6 @@
 USE world;
 --
-
+-- 1) Si vogliono recuperare dal database "world" la lingua e la nazione di ogni città.
 SELECT city.Name AS Info, country.Name AS Dettaglio
 FROM city
 JOIN country ON city.CountryCode = country.Code
@@ -10,7 +10,7 @@ FROM city
 JOIN countrylanguage ON city.CountryCode = countrylanguage.CountryCode;
 
 --
-
+-- 2) Si vuole recuperare il numero di città per nazione dal database "world" mostrando anche il nome della nazione e ordinarli in base al numero di città.
 SELECT country.Name AS Nazione, COUNT(city.ID) AS NumeroCitta
 FROM country
 JOIN city ON city.CountryCode = country.Code
@@ -21,7 +21,7 @@ FROM country;
 
     
 --
-
+-- 3) Si vuole conoscere la lista di repubbliche con aspettativa di vita maggiore dei 70 anni, inoltre si vuole visualizzare anche la lingua parlata.
 SELECT country.Name AS Nazione, countrylanguage.Language AS Lingua
 FROM country
 JOIN countrylanguage ON country.Code = countrylanguage.CountryCode
